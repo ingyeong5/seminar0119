99705ab636b0759b747a0c5d634036fcc42847c3
ingyeong5 <iloveig5@snu.ac.kr>
2026-01-19 11:02:23 +0900

    260119_1
    1. SweerAlert2 추가

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
+import Swal from "sweetalert2";
+
 const button = document.getElementById("danger-btn");
 
 button.addEventListener("click", () => {
-  alert("I Said Do Not Click This Button");
+  Swal.fire({
+  title: "⚠️ Warning",
+  text: "I Said Do Not Click This Button",
+  icon: "warning",
+  confirmButtonText: "Okay...",
+  background: "#0f2a4d",
+  color: "#e6ecf3",
+  confirmButtonColor: "#1d4b8f",
+  iconColor: "#ffcc00",
+  showClass: {
+    popup: "swal2-show swal2-animate-pop"
+  },
+  hideClass: {
+    popup: "swal2-hide swal2-animate-hide"
+  }
+});
 });
